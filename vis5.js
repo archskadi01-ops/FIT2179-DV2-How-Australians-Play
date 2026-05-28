9b0df9067cb3dab92d9deb90a98bd101662040bb
archskadi01-ops <archskadi01@gmail.com>
2026-05-28 23:53:31 +1000

    Create vis5.js

---
+++ vis5.js
@@ -0,0 +1,72 @@
+const chart5 = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+
+    "width": 350,
+    "height": 260,
+
+    "data": {
+        "values": [
+            {"benefit": "Create enjoyment", "value": 95},
+            {"benefit": "Bring joy", "value": 94},
+            {"benefit": "Stimulate the mind", "value": 94},
+            {"benefit": "Connect different people", "value": 86},
+            {"benefit": "Create awareness", "value": 81},
+            {"benefit": "Improve physical wellbeing", "value": 63}
+        ]
+    },
+
+    "mark": {
+        "type": "circle",
+        "opacity": 0.85
+    },
+
+    "encoding": {
+        "y": {
+            "field": "benefit",
+            "type": "nominal",
+            "sort": "-x",
+            "axis": {
+                "labelColor": "white",
+                "title": null
+            }
+        },
+
+        "x": {
+            "field": "value",
+            "type": "quantitative",
+            "axis": {
+                "labelColor": "white",
+                "title": "Adults who agree (%)",
+                "titleColor": "white"
+            }
+        },
+
+        "size": {
+            "field": "value",
+            "type": "quantitative",
+            "legend": null,
+            "scale": {
+                "range": [100, 900]
+            }
+        },
+
+        "color": {
+            "value": "#ffb000"
+        },
+
+        "tooltip": [
+            {"field": "benefit", "type": "nominal", "title": "Personal growth benefit"},
+            {"field": "value", "type": "quantitative", "title": "Percentage"}
+        ]
+    },
+
+    "config": {
+        "background": "transparent",
+        "view": {
+            "stroke": "transparent"
+        },
+        "axis": {
+            "gridColor": "#333"
+        }
+    }
+};

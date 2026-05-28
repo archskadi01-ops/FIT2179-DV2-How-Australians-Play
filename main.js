c6ba1d7a4ae9e2c884fe532a1d30f08000ea7d11
archskadi01-ops <archskadi01@gmail.com>
2026-05-28 23:50:45 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-vegaEmbed('#chart1', chart1);
+vegaEmbed('#chart1', chart1, {actions:false});
 
-vegaEmbed("#chart2", chart2);
+vegaEmbed("#chart2", chart2, {actions:false});
 
-vegaEmbed("#chart3", chart3);
+vegaEmbed("#chart3", chart3, {actions:false});
+
+vegaEmbed("#chart4", chart4, {actions:false});
+
+vegaEmbed("#chart5", chart5, {actions:false});
+
+vegaEmbed("#chart6", chart6, {actions:false});
+
+vegaEmbed("#chart7", chart7, {actions:false});
+
+vegaEmbed("#chart8", chart8, {actions:false});
+
+vegaEmbed("#chart9", chart9, {actions:false});
+
+vegaEmbed("#chart10", chart10, {actions:false});

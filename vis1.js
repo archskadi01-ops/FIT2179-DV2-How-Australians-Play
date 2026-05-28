d4268efada7dc3bc1d92a38cee36843e6785270d
archskadi01-ops <archskadi01@gmail.com>
2026-05-28 16:38:47 +1000

    Update vis1.js

--- vis1.js
+++ vis1.js
@@ -1,6 +1,9 @@
 const chart1 = {
     "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
 
+    "width": 300,
+    "height": 250,
+
     "data": {
         "values": [
             {"category":"Gamers","value":81},
@@ -8,10 +11,52 @@ const chart1 = {
         ]
     },
 
-    "mark":"bar",
+    "mark":{
+        "type":"bar",
+        "cornerRadiusTopLeft":10,
+        "cornerRadiusTopRight":10
+    },
 
     "encoding":{
-        "x":{"field":"category","type":"nominal"},
-        "y":{"field":"value","type":"quantitative"}
+        "x":{
+            "field":"category",
+            "type":"nominal",
+            "axis":{
+                "labelColor":"white",
+                "title":null
+            }
+        },
+
+        "y":{
+            "field":"value",
+            "type":"quantitative",
+            "axis":{
+                "labelColor":"white",
+                "title":"Percentage"
+            }
+        },
+
+        "color":{
+            "field":"category",
+            "scale":{
+                "range":["#ffb000","#444444"]
+            },
+            "legend":null
+        },
+
+        "tooltip":[
+            {"field":"category","type":"nominal"},
+            {"field":"value","type":"quantitative"}
+        ]
+    },
+
+    "config":{
+        "background":"transparent",
+        "view":{
+            "stroke":"transparent"
+        },
+        "axis":{
+            "gridColor":"#444"
+        }
     }
 };

a5c0f7884c59258dd8b93088feed77aae5304058
archskadi01-ops <archskadi01@gmail.com>
2026-05-29 03:50:03 +1000

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ vegaEmbed("#chart6", chart6, {actions:false});
 
 vegaEmbed("#chart7", chart7, {actions:false});
 
-vegaEmbed("#chart8", chart8, {actions:false});
+vegaEmbed("#vis8", mapSpec, {actions:false});
 
 vegaEmbed("#chart9", chart9, {actions:false});
 

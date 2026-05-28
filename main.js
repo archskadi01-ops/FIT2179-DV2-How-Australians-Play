2f633744d8e7eb17dd3c4cb0290a4962923a78c7
archskadi01-ops <archskadi01@gmail.com>
2026-05-29 04:02:29 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
-vegaEmbed('#chart1', chart1, {actions:false});
-
-vegaEmbed("#chart2", chart2, {actions:false});
-
-vegaEmbed("#chart3", chart3, {actions:false});
-
-vegaEmbed("#chart4", chart4, {actions:false});
-
-vegaEmbed("#chart5", chart5, {actions:false});
-
-vegaEmbed("#chart6", chart6, {actions:false});
-
-vegaEmbed("#chart7", chart7, {actions:false});
-
-vegaEmbed("#vis8", mapSpec, {actions:false});
-
-vegaEmbed("#chart9", chart9, {actions:false});
-
-vegaEmbed("#chart10", chart10, {actions:false});
+vegaEmbed("#chart1", chart1, {actions:false}).catch(console.error);
+vegaEmbed("#chart2", chart2, {actions:false}).catch(console.error);
+vegaEmbed("#chart3", chart3, {actions:false}).catch(console.error);
+vegaEmbed("#chart4", chart4, {actions:false}).catch(console.error);
+vegaEmbed("#chart5", chart5, {actions:false}).catch(console.error);
+vegaEmbed("#chart6", chart6, {actions:false}).catch(console.error);
+vegaEmbed("#chart7", chart7, {actions:false}).catch(console.error);
+vegaEmbed("#chart8", chart8, {actions:false}).catch(console.error);
+vegaEmbed("#chart9", chart9, {actions:false}).catch(console.error);
+vegaEmbed("#chart10", chart10, {actions:false}).catch(console.error);
